88440565bc161f3f82927f61b9015f3820898eec
Fabio <4898010+gatto@users.noreply.github.com>
2023-06-06 14:40:42 +0200

    Update mnist.py

--- src/mnist.py
+++ src/mnist.py
@@ -27,7 +27,7 @@ logging.basicConfig(
     filemode="a",
     format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
     datefmt="%H:%M:%S",
-    level=logging.DEBUG,
+    level=logging.WARNING,
 )
 
 
