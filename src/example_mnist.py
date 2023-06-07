5a9edb7f95b62ff094329bb8250c5c532636d55c
Fabio <4898010+gatto@users.noreply.github.com>
2023-06-07 13:05:05 +0200

    bugfix

--- src/example_mnist.py
+++ src/example_mnist.py
@@ -1,5 +1,5 @@
-from mnist import run_explain
 import sys
+
 from rich import print
 
 if __name__ == "__main__":
@@ -7,15 +7,18 @@ if __name__ == "__main__":
         run_options = sys.argv[1]
     except IndexError:
         raise Exception(
-            """possible runtime arguments are:\n
-            <start_index_image_to_explain>-<end_index_image_to_explain>\n
+            """possible runtime arguments are:
+            <start_index_image_to_explain>-<end_index_image_to_explain>
             example: python example_mnist.py 0-100"""
         )
+    from mnist import run_explain
 
-run_options = run_options.split("-")
-my_counter = 0
-for i in range(run_options[0], run_options[1]):
-    run_explain(i)
-    my_counter += 1
+    run_options = [int(x) for x in run_options.split("-")]
+    my_counter = 0
+    for i in range(run_options[0], run_options[1]):
+        run_explain(i)
+        my_counter += 1
 
-print(f"Explained instances from {run_options[0]} to {run_options[1]} amounting to {my_counter} instances.")
+    print(
+        f"Explained instances from {run_options[0]} to {run_options[1]} amounting to {my_counter} instances."
+    )
