ca73bd4ddcc70412176777292c27c51ab58a169a
Fabio <4898010+gatto@users.noreply.github.com>
2023-06-08 14:29:28 +0200

    A bit smarter script

--- src/example_mnist.py
+++ src/example_mnist.py
@@ -1,5 +1,5 @@
 import sys
-
+from parameters import path_aemodels
 from rich import print
 
 if __name__ == "__main__":
@@ -8,16 +8,22 @@ if __name__ == "__main__":
     except IndexError:
         raise Exception(
             """possible runtime arguments are:
-            <start_index_image_to_explain>-<end_index_image_to_explain>
-            example: python example_mnist.py 0-100"""
+            <how many images to explain>
+            example: python example_mnist.py 100
+            will explain 100 images not already explained"""
         )
     from mnist import run_explain, get_data
 
-    run_options = [int(x) for x in run_options.split("-")]
+    run_options = int(run_options)
     my_counter = 0
+    explanation_path = path_aemodels / "explanation"
+    # check what I've already done
+    max_i = int(max(explanation_path.glob("*.pickle")).stem)
+    
     (X_train, Y_train), (X_test, Y_test), (X_tree, Y_tree) = get_data()
 
-    for i in range(run_options[0], run_options[1]):
+    # do some more
+    for i in range(max_i+1, max_i+1+run_options):
         run_explain(i, X_tree, Y_tree)
         my_counter += 1
 
