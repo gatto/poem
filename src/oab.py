25b577fcb50077300a681ef648309fcd9ae4c027
Fabio <4898010+gatto@users.noreply.github.com>
2023-06-20 16:46:08 +0200

    Update oab.py

--- src/oab.py
+++ src/oab.py
@@ -121,9 +121,9 @@ class TestPoint:
         validator=validators.instance_of(np.ndarray),
         repr=lambda value: f"{type(value)}",
     )
-    latent: Latent = field(init=False)
     blackbox: Blackbox
     domain: Domain
+    latent: Latent = field()
 
     @latent.default
     def _latent_default(self):
