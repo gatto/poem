b20d186cece55b3e966499cd61508a7fbd89ba1b
Fabio <4898010+gatto@users.noreply.github.com>
2023-08-18 17:50:42 +0200

    Create exploration.py

---
+++ src/exploration.py
@@ -0,0 +1,24 @@
+import oab
+import matplotlib.pyplot as plt
+import logging
+
+logging.basicConfig(
+    filename="mnist-oab.log",
+    filemode="a",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.INFO,
+)
+
+(X_train, Y_train), (X_test, Y_test), (X_tree, Y_tree) = oab.get_data()
+
+my_dom = oab.Domain(dataset="mnist")
+
+# this gives problems: oab.TestPoint(X_test[200], domain=my_dom)
+points = X_test[:2]
+
+for i, array in enumerate(points):
+    logging.info(f"Point #{i} creation")
+    test_point = oab.TestPoint(array, my_dom)
+    logging.info(f"Point #{i} explanation")
+    exp = oab.Explainer(test_point)
