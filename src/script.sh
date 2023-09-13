75c918e3c58bed9059299ccfb6574d1d93bd4ebe
Fabio <4898010+gatto@users.noreply.github.com>
2023-09-13 14:48:56 +0200

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -4,7 +4,7 @@ cd ~/repos/tesi
 source venv/bin/activate
 cd src
 
-for ((i=5; i<5000; i = i + 5))
+for ((i=540; i<5000; i = i + 5))
 do
 	/home/frusso/miniconda3/condabin/conda run -n tesi /home/frusso/repos/tesi/venv/bin/python oab.py fashion dnn explain "$i"
 done
