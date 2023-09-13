b4d5673cbd3ec2c802b28bb39e674475c421206a
Fabio <4898010+gatto@users.noreply.github.com>
2023-09-13 14:58:20 +0200

    bugfix

--- src/script.sh
+++ src/script.sh
@@ -4,9 +4,9 @@ cd ~/repos/tesi
 source venv/bin/activate
 cd src
 
-for ((i=540; i<5000; i = i + 5))
+for ((i=545; i<5000; i = i + 5))
 do
-	/home/frusso/miniconda3/condabin/conda run -n tesi /home/frusso/repos/tesi/venv/bin/python oab.py fashion dnn explain "$i"
+	/home/frusso/miniconda3/condabin/conda run -n tesi /home/frusso/repos/tesi/venv/bin/python oab.py fashion dnn test-train "$i"
 done
 
 # conda run -n tesi python -c "from pathlib import Path; print(Path.cwd())"
