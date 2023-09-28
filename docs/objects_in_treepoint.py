289ea4192ffeb15a695911cb50276459e0999d61
Fabio <4898010+gatto@users.noreply.github.com>
2023-09-28 23:23:03 +0200

    Update objects_in_treepoint.py
    Co-Authored-By: Carlo Metta <43177512+MistyDay86@users.noreply.github.com>

--- docs/objects_in_treepoint.py
+++ docs/objects_in_treepoint.py
@@ -15,23 +15,48 @@ relationships = [
     ("ComplexRule", "Condition", {"what": "input"}),  # several
 ]
 
-#    ("TreePoint", "id: int", {"what": "input"}),
-#    ("TreePoint", "a: np.ndarray", {"what": "input"}),
-#    ("LatentDT", "predicted_class: str", {"what": "computed"}),
-#    ("LatentDT", "fidelity: float", {"what": "computed"}),
-#    ("LatentDT", "model: sklearn.DecisionTreeClassifier", {"what": "computed"}),
-#    ("Domain", "dataset_name", {"what": "input"}),
-#    ("Domain", "bb_type", {"what": "input"}),
-#    ("Domain", "metadata", {"what": "input"}),
-#    ("Domain", "classes", {"what": "input"}),
-#    ("Latent", "a: np.ndarray", {"what": "input"}),
-#    ("Latent", "margins: np.ndarray", {"what": "input"}),
-
 G.add_edges_from(relationships)
 
+# Define custom positions
+pos = {
+    "TreePoint": (-1, -1.5),
+    "Domain": (0, -1),
+    "Latent": (0, -2),
+    "LatentDT": (0, 0),
+    "BlackboxPD": (0, -3),
+    "ComplexRule": (1, 0),
+    "AE": (1, -2),
+    "Blackbox": (1, -1),
+    "explanation_base": (1, -3),
+    "Condition": (2, 0),
+}
 
-# draw it
-subax1 = plt.subplot(121)
-nx.draw_shell(G, with_labels=True)
+fig = plt.figure(1, figsize=(10, 4))
 
+# Draw the graph
+nx.draw(
+    G,
+    pos,
+    with_labels=False,
+    node_size=700,
+    node_color="orangered",
+    font_size=10,
+    font_weight="bold",
+)
+
+# Adjust label positions
+label_pos = {
+    "TreePoint": (-1.1, -1.25),
+    "Domain": (0, -0.75),
+    "Latent": (0, -1.75),
+    "LatentDT": (-0.23, 0),
+    "BlackboxPD": (0.12, -2.75),
+    "ComplexRule": (1, -0.3),
+    "AE": (1.13, -2),
+    "Blackbox": (1.23, -1),
+    "explanation_base": (1.35, -3),
+    "Condition": (2, -0.3),
+}
+
+nx.draw_networkx_labels(G, label_pos)
 plt.show()
