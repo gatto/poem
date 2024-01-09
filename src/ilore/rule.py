76bd6e8c00603462244efcbbfb22da3e05d64320
Fabio <4898010+gatto@users.noreply.github.com>
2024-01-09 17:09:31 +0100

    sigh

--- src/ilore/rule.py
+++ src/ilore/rule.py
@@ -143,8 +143,9 @@ def get_rule(x, dt, feature_names, class_name, class_values, numeric_columns):
 
     logging.warning(f"{class_values=}")
     logging.warning(f"{dt_outcome=}")
+    logging.warning(f"{type(dt_outcome)}")
     logging.warning("10")
-    cons = class_values[class_values.index(dt_outcome)]
+    cons = class_values[class_values.index(str(dt_outcome))]
     logging.warning("11")
     premises = compact_premises(premises)
     logging.warning("12")
