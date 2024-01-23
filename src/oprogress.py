3b2cf822a288128e4788bd57494634b67eb6d570
Fabio <4898010+gatto@users.noreply.github.com>
2024-01-23 18:28:25 +0100

    bugfix

--- src/oprogress.py
+++ src/oprogress.py
@@ -4,15 +4,15 @@ from rich.progress import Progress
 from watchfiles import watch
 
 if __name__ == "__main__":
-    print("Ctrl-c to quit")
+    print("[gray]Ctrl-c to quit")
     with Progress() as progress:
         task1 = progress.add_task("[purple]👀...", total=None)
 
         for _ in watch("data/progress/progr.pickle"):
             with open("data/progress/progr.pickle", "rb") as f:
                 d = pickle.load(f)
-            if d.good:
-                progress.update(task1, total=d.total, advance=1)
+            if d["good"]:
+                progress.update(task1, total=d["total"], advance=1)
             else:
                 progress.stop()
                 print("[red]ERROR.")
