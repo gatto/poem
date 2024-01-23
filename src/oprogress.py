487555e67e63f3b8f2ea576a4fdb636088258d7e
Fabio <4898010+gatto@users.noreply.github.com>
2024-01-23 18:33:22 +0100

    Update oprogress.py

--- src/oprogress.py
+++ src/oprogress.py
@@ -1,18 +1,29 @@
 import pickle
 
-from rich.progress import Progress
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TextColumn,
+    MofNCompleteColumn,
+    TimeRemainingColumn,
+)
 from watchfiles import watch
 
 if __name__ == "__main__":
-    print("[gray]Ctrl-c to quit")
-    with Progress() as progress:
+    print("[grey]Ctrl-c to quit")
+    with Progress(
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TimeRemainingColumn(),
+    ) as progress:
         task1 = progress.add_task("[purple]👀...", total=None)
 
         for _ in watch("data/progress/progr.pickle"):
             with open("data/progress/progr.pickle", "rb") as f:
                 d = pickle.load(f)
             if d["good"]:
-                progress.update(task1, total=d["total"], advance=1)
+                progress.update(task1, total=d["total"], completed=d["current"])
             else:
                 progress.stop()
                 print("[red]ERROR.")
