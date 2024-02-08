dc9c503c5250798151076eb7fe654a2f342f628e
Fabio <4898010+gatto@users.noreply.github.com>
2024-02-08 15:06:38 +0100

    lambda layer fix attempt

--- src/abele/autoencoder.py
+++ src/abele/autoencoder.py
@@ -4,27 +4,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import model_from_json
 
+
 def save(model, model_name, path):
-    model_filename = '%s%s.json' % (path, model_name)
-    weights_filename = '%s%s_weights.hdf5' % (path, model_name)
-    options = {'file_arch': model_filename, 'file_weight': weights_filename}
+    model_filename = "%s%s.json" % (path, model_name)
+    weights_filename = "%s%s_weights.hdf5" % (path, model_name)
+    options = {"file_arch": model_filename, "file_weight": weights_filename}
     json_string = model.to_json()
-    open(options['file_arch'], 'w').write(json_string)
-    model.save_weights(options['file_weight'])
+    open(options["file_arch"], "w").write(json_string)
+    model.save_weights(options["file_weight"])
 
 
 def load(model_name, path):
     model_filename = "%s%s.json" % (path, model_name)
     weights_filename = "%s%s_weights.hdf5" % (path, model_name)
-    model = model_from_json(open(model_filename, 'r').read())
+    model = model_from_json(open(model_filename, "r").read(), safe_mode=False)
     model.load_weights(weights_filename)
     return model
 
 
 class Autoencoder(object):
 
-    def __init__(self, shape, input_dim, latent_dim=4, hidden_dim=1024, alpha=0.2, verbose=False,
-                 store_intermediate=False, save_graph=False, path='./', name='aae'):
+    def __init__(
+        self,
+        shape,
+        input_dim,
+        latent_dim=4,
+        hidden_dim=1024,
+        alpha=0.2,
+        verbose=False,
+        store_intermediate=False,
+        save_graph=False,
+        path="./",
+        name="aae",
+    ):
         self.shape = shape
         self.input_dim = input_dim
         self.latent_dim = latent_dim
@@ -77,7 +89,7 @@ class Autoencoder(object):
     def sample_images(self, epoch):
         r, c = 5, 5
 
-        z = np.random.normal(size=(r*c, self.latent_dim))
+        z = np.random.normal(size=(r * c, self.latent_dim))
         Z = self.decoder.predict(z)
         Z = self.img_denormalize(Z)
 
@@ -86,30 +98,29 @@ class Autoencoder(object):
         for i in range(r):
             for j in range(c):
                 if self.shape[2] == 1:
-                    axs[i, j].imshow(Z[cnt].reshape(self.shape), cmap='gray')
+                    axs[i, j].imshow(Z[cnt].reshape(self.shape), cmap="gray")
                 else:
                     axs[i, j].imshow(Z[cnt].reshape(self.shape))
-                axs[i, j].axis('off')
+                axs[i, j].axis("off")
                 cnt += 1
-        fig.savefig('%s%s_%d.png' % (self.path, self.name, epoch))
+        fig.savefig("%s%s_%d.png" % (self.path, self.name, epoch))
         plt.close()
 
     def save_model(self):
-        save(self.encoder, '%s_encoder' % self.name, self.path)
-        save(self.decoder, '%s_decoder' % self.name, self.path)
-        save(self.autoencoder, '%s_autoencoder' % self.name, self.path)
+        save(self.encoder, "%s_encoder" % self.name, self.path)
+        save(self.decoder, "%s_decoder" % self.name, self.path)
+        save(self.autoencoder, "%s_autoencoder" % self.name, self.path)
 
         if self.discriminator is not None:
-            save(self.discriminator, '%s_discriminator' % self.name, self.path)
+            save(self.discriminator, "%s_discriminator" % self.name, self.path)
         if self.generator is not None:
-            save(self.generator, '%s_generator' % self.name, self.path)
+            save(self.generator, "%s_generator" % self.name, self.path)
 
     def load_model(self):
-        self.encoder = load('%s_encoder' % self.name, self.path)
-        self.decoder = load('%s_decoder' % self.name, self.path)
-        self.autoencoder = load('%s_autoencoder' % self.name, self.path)
-        if 'aae' in self.name or 'aag' in self.name:
-            self.discriminator = load('%s_discriminator' % self.name, self.path)
-        if 'aag' in self.name:
-            self.generator = load('%s_generator' % self.name, self.path)
-
+        self.encoder = load("%s_encoder" % self.name, self.path)
+        self.decoder = load("%s_decoder" % self.name, self.path)
+        self.autoencoder = load("%s_autoencoder" % self.name, self.path)
+        if "aae" in self.name or "aag" in self.name:
+            self.discriminator = load("%s_discriminator" % self.name, self.path)
+        if "aag" in self.name:
+            self.generator = load("%s_generator" % self.name, self.path)
